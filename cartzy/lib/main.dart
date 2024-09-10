f30489b708597712d186402ed4c1ad7bd5d3c70b
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-11 01:24:56 +0500

    Hardcode styling

--- cartzy/lib/main.dart
+++ cartzy/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {}
+void main(List<String> args) {
+  runApp(mtyclass());
+}
 
 class mtyclass extends StatelessWidget {
   const mtyclass({super.key});
@@ -11,6 +13,47 @@ class mtyclass extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: ThemeData(),
       darkTheme: ThemeData(),
+      home: myapp(),
+    );
+  }
+}
+
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("My Theme Application"),
+      ),
+      body: const Column(
+        children: [
+          Text(
+            "Hello World",
+            style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Hello World",
+            style: TextStyle(
+                fontSize: 11,
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic),
+          ),
+          Text(
+            "Hello World",
+            style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Hello World",
+            style: TextStyle(
+                fontSize: 11,
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic),
+          ),
+        ],
+      ),
     );
   }
 }
