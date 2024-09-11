0b9bbc5c2e648d04a76f08af82639b36fa949097
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-11 14:05:13 +0500

    setting up all the theme styling of text in light mode

--- cartzy/lib/utils/theme/custom theme/text_theme.dart
+++ cartzy/lib/utils/theme/custom theme/text_theme.dart
@@ -7,18 +7,21 @@ class CTextTheme {
     headlineLarge: const TextStyle().copyWith(fontSize: 32.0, fontWeight: FontWeight.bold),
     headlineMedium: const TextStyle().copyWith(fontSize: 24.0, fontWeight: FontWeight.w600),
     headlineSmall: const TextStyle().copyWith(fontSize: 18.0, fontWeight: FontWeight.w600),
-
     //
 
     titleLarge: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w600),
     titleMedium: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w500),
     titleSmall: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w400),
-
     //
 
     bodyLarge: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.w500),
     bodyMedium: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.normal),
     bodySmall: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.w500),
+    //
+
+    labelLarge: const TextStyle().copyWith(fontSize: 12.0, fontWeight: FontWeight.normal),
+    labelMedium: const TextStyle().copyWith(fontSize: 12.0, fontWeight: FontWeight.normal),
+    //
   );
   //
   static TextTheme darkTextTheme = const TextTheme(
