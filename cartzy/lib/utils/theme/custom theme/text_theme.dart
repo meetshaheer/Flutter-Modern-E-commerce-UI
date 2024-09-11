691a0f83da97b608cae2456bd1f4b12209cb953f
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-11 14:07:26 +0500

    also setting up the dark theme of the text

--- cartzy/lib/utils/theme/custom theme/text_theme.dart
+++ cartzy/lib/utils/theme/custom theme/text_theme.dart
@@ -24,17 +24,22 @@ class CTextTheme {
     //
   );
   //
-  static TextTheme darkTextTheme = const TextTheme(
-    displayLarge: TextStyle(
-      fontSize: 21,
-      color: Colors.white,
-      fontWeight: FontWeight.bold,
-    ),
-    displayMedium: TextStyle(
-      fontSize: 15,
-      color: Colors.white,
-      fontWeight: FontWeight.bold,
-      fontStyle: FontStyle.italic,
-    ),
+  static TextTheme darkTextTheme = TextTheme(
+    headlineLarge: const TextStyle().copyWith(fontSize: 32.0, fontWeight: FontWeight.bold),
+    headlineMedium: const TextStyle().copyWith(fontSize: 24.0, fontWeight: FontWeight.w600),
+    headlineSmall: const TextStyle().copyWith(fontSize: 18.0, fontWeight: FontWeight.w600),
+    //
+
+    titleLarge: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w600),
+    titleMedium: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w500),
+    titleSmall: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w400),
+    //
+    bodyLarge: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.w500),
+    bodyMedium: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.normal),
+    bodySmall: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.w500),
+    //
+
+    labelLarge: const TextStyle().copyWith(fontSize: 12.0, fontWeight: FontWeight.normal),
+    labelMedium: const TextStyle().copyWith(fontSize: 12.0, fontWeight: FontWeight.normal),
   );
 }
