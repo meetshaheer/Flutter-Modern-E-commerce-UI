4964ca4d65a57332b0cb31dd2004c1b729f34b64
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-16 13:47:26 +0500

    updated with screens logic

--- worncom/lib/components/bottomAppBar.dart
+++ worncom/lib/components/bottomAppBar.dart
@@ -21,17 +21,41 @@ class _bottomAppBarState extends State<bottomAppBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: NavigationBar(
-      onDestinationSelected: (value) => selectedIndex(value),
-      selectedIndex: _selectedIndex,
-      height: 80,
-      elevation: 0,
-      destinations: const [
-        NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
-        NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
-        NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
-        NavigationDestination(icon: Icon(Iconsax.user), label: "Account"),
-      ],
-    ));
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (value) => selectedIndex(value),
+        selectedIndex: _selectedIndex,
+        height: 80,
+        elevation: 0,
+        destinations: const [
+          NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
+          NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
+          NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
+          NavigationDestination(icon: Icon(Iconsax.user), label: "Account"),
+        ],
+      ),
+      body: MyWidgets().screen[_selectedIndex],
+    );
+  }
+}
+
+class MyWidgets extends StatelessWidget {
+  MyWidgets({super.key});
+  List screen = [
+    Container(
+      color: Colors.amber,
+    ),
+    Container(
+      color: const Color.fromARGB(255, 12, 173, 146),
+    ),
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: const Color.fromARGB(255, 255, 121, 165),
+    )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
