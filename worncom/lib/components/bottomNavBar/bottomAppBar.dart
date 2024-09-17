5ca7bfd4504ed5b6854ea9b341234f47d6bbc7dc
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-18 02:27:41 +0500

    updated with some changings

--- worncom/lib/components/bottomNavBar/bottomAppBar.dart
+++ worncom/lib/components/bottomNavBar/bottomAppBar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:worncome/screens/login/login.dart';
 import 'package:worncome/utils/constants/colors.dart';
 import 'package:worncome/utils/theme/theme.dart';
 
@@ -32,7 +33,10 @@ class _bottomAppBarState extends State<bottomAppBar> {
       elevation: 0,
       destinations: const [
         NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
-        NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
+        NavigationDestination(
+          icon: Icon(Iconsax.shop),
+          label: "Store",
+        ),
         NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
         NavigationDestination(icon: Icon(Iconsax.user), label: "Account"),
       ],
